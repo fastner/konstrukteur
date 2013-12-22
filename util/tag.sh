c558f054755f84cffd15615e50ffe204a9798593
Sebastian Fastner <mail@sebastianfastner.de>
2013-12-22 22:49:29 +0100

    Fix version detection from jasy to konstrukteur

--- util/tag.sh
+++ util/tag.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd `dirname $0`/..
-version=`python3 -c "import jasy; print(jasy.__version__)"`
+version=`python3 -c "import konstrukteur; print(konstrukteur.__version__)"`
 
 git tag -d $version
 git tag -a $version -m "Release of version $version"
