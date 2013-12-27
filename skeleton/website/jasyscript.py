18998011bb52616a3002ca298a64ea61c5727a76
Sebastian Fastner <mail@sebastianfastner.de>
2013-12-27 19:18:45 +0100

    Copy used assets to output path

--- skeleton/website/jasyscript.py
+++ skeleton/website/jasyscript.py
@@ -6,7 +6,10 @@ def build(regenerate = False):
 	"""Generate source (development) version"""
 
 	# Initialize assets
-	AssetManager.AssetManager(profile, session)
+	assetManager = AssetManager.AssetManager(profile, session)
 
 	# Build static website
 	konstrukteur.Konstrukteur.build(regenerate)
+
+	# Copy assets to build path
+	assetManager.copyAssets()
