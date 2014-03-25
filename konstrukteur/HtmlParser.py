cadf94e5551759a94f8cce91b5b79e335509dfc2
Sebastian Werner <info@sebastian-werner.net>
2014-03-25 17:53:01 +0100

    Bumped years in copyright + added myself :)

--- konstrukteur/HtmlParser.py
+++ konstrukteur/HtmlParser.py
@@ -1,6 +1,7 @@
 #
 # Konstrukteur - Static website generator
-# Copyright 2013 Sebastian Fastner
+# Copyright 2013-2014 Sebastian Fastner
+# Copyright 2014 Sebastian Werner
 #
 
 __all__ = ["parse"]
